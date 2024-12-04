b4284430de158de895863e78649afa256560b808
ogua ahmed <ogua.ahmed18@gmail.com>
2024-12-04 20:03:42 +0000

    updated site|

--- our-service.php
+++ our-service.php
@@ -2,100 +2,99 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>Gracedew International School | Our Services</title>
+<meta charset="utf-8">
+<title>Gracedew International School | Our Services</title>
 
-  <meta name="description" content="Our services includes Regular Admission,Summer School,Online School,After School Club,Saturday Care,Extra- Curricular Activities(Super Readers Club, Swimming, Tennis, Ballet, Abacus with BraiNest).">
+<meta name="description" content="Our services includes Regular Admission,Summer School,Online School,After School Club,Saturday Care,Extra- Curricular Activities(Super Readers Club, Swimming, Tennis, Ballet, Abacus with BraiNest).">
 
-  <meta name="keywords" content="Gracedew International School, Gracedew, About Gracedew, About Gracedew International, Schools at Teshie, Gracedew, about Gracedew, Gracedew gallary, Gracedew images, Gracedew videos, Gracedew events, Gracedew school system, oguse it solutions, oguses, OguSesITSolutions, oguaschoolz">
+<meta name="keywords" content="Gracedew International School, Gracedew, About Gracedew, About Gracedew International, Schools at Teshie, Gracedew, about Gracedew, Gracedew gallary, Gracedew images, Gracedew videos, Gracedew events, Gracedew school system, oguse it solutions, oguses, OguSesITSolutions, oguaschoolz">
 
-  <link rel="canonical" href="https://wonderworldinternationalschool.com/our-service.php"/>
+<link rel="canonical" href="https://wonderworldinternationalschool.com/our-service.php"/>
 
-  <meta property="og:description" content="Our services includes Regular Admission,Summer School,Online School,After School Club,Saturday Care,Extra- Curricular Activities(Super Readers Club, Swimming, Tennis, Ballet, Abacus with BraiNest)." />
+<meta property="og:description" content="Our services includes Regular Admission,Summer School,Online School,After School Club,Saturday Care,Extra- Curricular Activities(Super Readers Club, Swimming, Tennis, Ballet, Abacus with BraiNest)." />
 
-  <meta property="og:title" content="Gracedew International School | Our Services" />
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://wonderworldinternationalschool.com/our-service.php" />
-  <meta property="og:image" content="https://wonderworldinternationalschool.com/assets/images/banner.jpg" />
+<meta property="og:title" content="Gracedew International School | Our Services" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://wonderworldinternationalschool.com/our-service.php" />
+<meta property="og:image" content="https://wonderworldinternationalschool.com/assets/images/banner.jpg" />
 
 
-  <meta name="twitter:title" content="Gracedew International School | Our Services" />
-  <meta name="twitter:site" content="@blogogua" />
-  <meta name="twitter:image" content="https://wonderworldinternationalschool.com/assets/images/banner.jpg" />
-  <meta name="twitter:description" content="Our services includes Regular Admission,Summer School,Online School,After School Club,Saturday Care,Extra- Curricular Activities(Super Readers Club, Swimming, Tennis, Ballet, Abacus with BraiNest)." />
+<meta name="twitter:title" content="Gracedew International School | Our Services" />
+<meta name="twitter:site" content="@blogogua" />
+<meta name="twitter:image" content="https://wonderworldinternationalschool.com/assets/images/banner.jpg" />
+<meta name="twitter:description" content="Our services includes Regular Admission,Summer School,Online School,After School Club,Saturday Care,Extra- Curricular Activities(Super Readers Club, Swimming, Tennis, Ballet, Abacus with BraiNest)." />
 
-  <script type="application/ld+json">{"@context":"https://schema.org","@type":"WebPage","name":"Gracedew International School | Our Services","description":"Our services includes Regular Admission,Summer School,Online School,After School Club,Saturday Care,Extra- Curricular Activities(Super Readers Club, Swimming, Tennis, Ballet, Abacus with BraiNest).","image":"https://wonderworldinternationalschool.com/assets/images/banner.jpg"}</script>
-    
-    <link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/ico">
-    
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<script type="application/ld+json">{"@context":"https://schema.org","@type":"WebPage","name":"Gracedew International School | Our Services","description":"Our services includes Regular Admission,Summer School,Online School,After School Club,Saturday Care,Extra- Curricular Activities(Super Readers Club, Swimming, Tennis, Ballet, Abacus with BraiNest).","image":"https://wonderworldinternationalschool.com/assets/images/banner.jpg"}</script>
 
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@600&family=Lobster+Two:wght@700&display=swap" rel="stylesheet">
-    
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+<link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/ico">
 
-    <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Google Web Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@600&family=Lobster+Two:wght@700&display=swap" rel="stylesheet">
 
-    <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
+<!-- Icon Font Stylesheet -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- Libraries Stylesheet -->
+<link href="lib/animate/animate.min.css" rel="stylesheet">
+<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+<!-- Customized Bootstrap Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Template Stylesheet -->
+<link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="container-xxl bg-white p-0">
-        <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div>
-        <!-- Spinner End -->
+<div class="container-xxl bg-white p-0">
+<!-- Spinner Start -->
+<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+<div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+<span class="sr-only">Loading...</span>
+</div>
+</div>
+<!-- Spinner End -->
 
 
-        <!-- Navbar Start -->
-        <?php include('navbar.php') ?>
-        <!-- Navbar End -->
+<!-- Navbar Start -->
+<?php include('navbar.php') ?>
+<!-- Navbar End -->
 
 
-        <!-- Page Header End -->
-        <div class="container-xxl py-5 page-header position-relative mb-5">
-            <div class="container py-5">
-                <h1 class="display-2 text-white animated slideInDown mb-4">Our services</h1>
-                <nav aria-label="breadcrumb animated slideInDown">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">About Us</a></li>
-                        <li class="breadcrumb-item text-white active" aria-current="page">Our services</li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-        <!-- Page Header End -->
+<!-- Page Header End -->
+<div class="container-xxl py-5 page-header position-relative mb-5">
+<div class="container py-5">
+<h1 class="display-2 text-white animated slideInDown mb-4">Our services</h1>
+<nav aria-label="breadcrumb animated slideInDown">
+<ol class="breadcrumb">
+<li class="breadcrumb-item"><a href="#">Home</a></li>
+<li class="breadcrumb-item"><a href="#">About Us</a></li>
+<li class="breadcrumb-item text-white active" aria-current="page">Our services</li>
+</ol>
+</nav>
+</div>
+</div>
+<!-- Page Header End -->
 
-         <!-- Classes Start -->
-        <div class="container-xxl py-5">
-            <div class="container">
-                <p class="wow fadeInUp" data-wow-delay="0.2s">At Gracedew International School, we
+<!-- Classes Start -->
+<div class="container-xxl py-5">
+<div class="container">
+<p class="wow fadeInUp" data-wow-delay="0.2s">At Gracedew International School, we
 operate under the Social Welfare and the
 Ghana Education Service (GES) and are
 known for delivering a high-quality
-service. <br> You should expect the following
-from our service:
+service. <br> Our services include:
 <ul>
-<li> Quality Staff </li>
-<li> Security </li>
-<li> Quality of Care </li>
-<li> Neat Premises </li>
-<li> Recommended Equipment </li>
+<li> Preschool - JHS</li>
+<li> Club activities </li>
+<li> Computer and Science labs</li>
+<li> Counseling and guidance</li>
+<li> Field Trip and excursions</li>
 </ul>
 
 </p>
@@ -107,32 +106,32 @@ from our service:
 <p class="wow fadeInUp" data-wow-delay="0.2s"> </p>
 
 
-                
-            </div>
-        </div>
-        <!-- Classes End -->
 
-        
-        <!-- Footer Start -->
-        <?php
-          include('footer.php');
-        ?>
-        <!-- Footer End -->
+</div>
+</div>
+<!-- Classes End -->
 
-        <!-- Back to Top -->
-        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-    </div>
 
-    <!-- JavaScript Libraries -->
-    <script src="/js/jquery-3.4.1.min.js"></script>
-    <script src="/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<!-- Footer Start -->
+<?php
+include('footer.php');
+?>
+<!-- Footer End -->
 
-    <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+<!-- Back to Top -->
+<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+</div>
+
+<!-- JavaScript Libraries -->
+<script src="/js/jquery-3.4.1.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
+<script src="lib/wow/wow.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
 </body>
 
 </html>
--- welcome-message.php
+++ welcome-message.php
@@ -1,89 +1,95 @@
 <!-- About Start -->
 <div class="container-xxl py-5">
-    <div class="container">
-        <div class="row g-5 avlign-items-center">
-            <div class="col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
-                <h1 class="mb-4 text-center">Our Welcome message</h1>
-                <p>We are delighted that you have enrolled
-                    your precious child with us here at
-                    Gracedew International School. We are
-                    committed to making your child's
-                    transition from home to school as easy
-                as possible..</p>
-                <p class="mb-4">To provide an efficient service we ask
-                    that all parents adhere to the following: <br>
-                    <ul>
-                        <li> Parents are asked to observe the
-                            school opening and closing hours of
-                            7.00 am till 4.00 pm. The school gate
-                            closes at exactly 8am and no child
-                            will be allowed in after the gate is
-                            closed. Late pickups warrant a
-                            penalty of GHc20.00 per hour, or part
-                        thereof. </li>
+<div class="container">
+<div class="row g-5 avlign-items-center">
+<div class="col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
+<h1 class="mb-4 text-center">Welcome to Gracedew International School</h1>
+<p>We're thrilled you're interested in joining our vibrant community of learners.</p>
 
-                        <li> For safety reasons, parents or
-                            guardians are asked to accompany
-                            their children to and from the school
-                            and hand them directly to a staff
-                            member. If a child is being picked up
-                            by anyone other than a parent or
-                        guardian, the staff must be notified. </li>
+<p>At gracedew, we empower students to achieve academic excellence, develop character, and become global citizens.</p>
+<p>Explore our site to discover how we nurture minds and inspire futures</p>
 
-                        <li> Parents must notify the school
-                            before 7:30 am if their ward will be
-                            late. Note that “traffic” is not an
-                        excuse. </li>
+<!-- <p>We are delighted that you have enrolled
+your precious child with us here at
+Gracedew International School. We are
+committed to making your child's
+transition from home to school as easy
+as possible..</p>
+<p class="mb-4">To provide an efficient service we ask
+that all parents adhere to the following: <br>
+<ul>
+<li> Parents are asked to observe the
+school opening and closing hours of
+7.00 am till 4.00 pm. The school gate
+closes at exactly 8am and no child
+will be allowed in after the gate is
+closed. Late pickups warrant a
+penalty of GHc20.00 per hour, or part
+thereof. </li>
 
-                        <li> Parents should provide water and
-                        fruit for their wards. </li>
+<li> For safety reasons, parents or
+guardians are asked to accompany
+their children to and from the school
+and hand them directly to a staff
+member. If a child is being picked up
+by anyone other than a parent or
+guardian, the staff must be notified. </li>
 
-                        <li> Children should not bring in their
-                        toys. </li>
+<li> Parents must notify the school
+before 7:30 am if their ward will be
+late. Note that “traffic” is not an
+excuse. </li>
 
-                        <li> All prescribed uniforms, sweaters,
-                            bags and lunch boxes should be
-                            clearly labelled with the child's
-                        name. </li>
+<li> Parents should provide water and
+fruit for their wards. </li>
 
-                        <li> Parents must pay the fees in full, or
-                            at least half of the fees before the
-                            start of every term. The remaining
-                            balance of the term’s fees must be
-                        paid at the end of the same month. </li>
+<li> Children should not bring in their
+toys. </li>
 
-                        <li>Parents must provide two house
-                            clothes and a pair of sandals that will
-                            be kept in the school for pupils in
-                            créche. On Thursday, the clothes,
-                            together with the PE attire, will be
-                            taken home for washing if they are
-                        dirty. </li>
+<li> All prescribed uniforms, sweaters,
+bags and lunch boxes should be
+clearly labelled with the child's
+name. </li>
 
-                        <li> If you have any questions about any
-                            staff member, our headmaster will
-                            be glad to discuss them with you in
-                        the strictest of confidence. </li>
+<li> Parents must pay the fees in full, or
+at least half of the fees before the
+start of every term. The remaining
+balance of the term’s fees must be
+paid at the end of the same month. </li>
 
-                        <li> If a child is unwell, he or she should
-                        stay home. </li>
-                    </ul>
-                </p>
-                <p class="mb-4">Finally, we welcome your child and hope
-                    he or she will blossom, grow, gain
-                    confidence, strengthen their self-
-                    esteem and independence and above all,
-                have fun and enjoy the experience..</p>
-                
-                        <!-- <div class="row g-4 align-items-center">
-                            <div class="col-sm-6">
-                                <a class="btn btn-custom rounded-pill py-3 px-5" href="about-us.php">Read More</a>
-                            </div>
-                            
-                        </div> -->
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-        <!-- About End -->
+<li>Parents must provide two house
+clothes and a pair of sandals that will
+be kept in the school for pupils in
+créche. On Thursday, the clothes,
+together with the PE attire, will be
+taken home for washing if they are
+dirty. </li>
+
+<li> If you have any questions about any
+staff member, our headmaster will
+be glad to discuss them with you in
+the strictest of confidence. </li>
+
+<li> If a child is unwell, he or she should
+stay home. </li>
+</ul>
+</p>
+
+<p class="mb-4">Finally, we welcome your child and hope
+he or she will blossom, grow, gain
+confidence, strengthen their self-
+esteem and independence and above all,
+have fun and enjoy the experience..</p> -->
+
+<!-- <div class="row g-4 align-items-center">
+<div class="col-sm-6">
+<a class="btn btn-custom rounded-pill py-3 px-5" href="about-us.php">Read More</a>
+</div>
+
+</div> -->
+</div>
+
+</div>
+</div>
+</div>
+<!-- About End -->
